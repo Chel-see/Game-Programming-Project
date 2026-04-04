de6d7af4c0b3cfa046c8e207dce4c9f1ddec2497
Chel-see <157260979+Chel-see@users.noreply.github.com>
2026-04-04 01:16:21 -0400

    Add files via upload

---
+++ BackgroundManager.java
@@ -0,0 +1,64 @@
+/* BackgroundManager manages many backgrounds (wraparound images 
+   used for the game's background). 
+
+   Backgrounds 'further back' move slower than ones nearer the
+   foreground of the game, creating a parallax distance effect.
+
+   When a sprite is instructed to move left or right, the sprite
+   doesn't actually move, instead the backgrounds move in the 
+   opposite direction (right or left).
+
+*/
+
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+
+public class BackgroundManager {
+
+    private String bgImages[] = {"Background/Layers/1.png","Background/Layers/2.png","Background/Layers/3.png","Background/Layers/4.png","Background/Layers/5.png"};
+
+      private int moveAmount[] = {1, 2, 3, 4,4};  
+                        // pixel amounts to move each background left or right
+                             // a move amount of 0 makes a background stationary
+
+      private Background[] backgrounds;
+      private int numBackgrounds;
+
+      private JPanel panel;            // JPanel on which backgrounds are drawn
+
+      public BackgroundManager(JPanel panel, int moveSize) {
+                        // ignore moveSize
+            this.panel = panel;
+
+            numBackgrounds = bgImages.length;
+            backgrounds = new Background[numBackgrounds];
+
+            for (int i = 0; i < numBackgrounds; i++) {
+                   backgrounds[i] = new Background(panel, bgImages[i], moveAmount[i]);
+            }
+      } 
+
+
+      public void moveRight() { 
+        for (int i=0; i < numBackgrounds; i++)
+                  backgrounds[i].moveRight();
+      }
+
+
+      public void moveLeft() {
+        for (int i=0; i < numBackgrounds; i++)
+                  backgrounds[i].moveLeft();
+      }
+
+
+      // The draw method draws the backgrounds on the screen. The
+      // backgrounds are drawn from the back to the front.
+
+      public void draw (Graphics2D g2) { 
+        for (int i=0; i < numBackgrounds; i++)
+                  backgrounds[i].draw(g2);
+      }
+
+}
+
